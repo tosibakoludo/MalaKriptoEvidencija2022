3dbcf0fe680a33091c158f169e740391cc8a5d7c
Tamara Brkic <brkictamara7@gmail.com>
2022-03-19 12:12:09 +0100

    Dodata klasa Korisnik.

---
+++ Korisnik.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalaKriptoEvidencija2022
+{
+    class Korisnik
+    {
+        private string ime, prezime;
+
+        private List<Ulaganje> listaUlaganja;
+
+        public Korisnik(string ime, string prezime)
+        {
+            this.Ime = ime;
+            this.Prezime = prezime;
+            listaUlaganja = new List<Ulaganje>();
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public List<Ulaganje> ListaUlaganja { get => listaUlaganja; }
+
+        //indekser
+        public Ulaganje this[int index]
+        {
+            get
+            {
+                return listaUlaganja[index];
+            }
+            set
+            {
+                listaUlaganja[index] = value;
+            }
+        }
+
+        public void dodaj(Ulaganje ulaganje)
+        {
+            listaUlaganja.Add(ulaganje);
+        }
+
+        public int redniBroj()
+        {
+            return listaUlaganja.Count + 1;
+        }
+
+        public void obrisi()
+        {
+            listaUlaganja.Clear();
+        }
+
+        public string valutaSaNajvecimUlogom()
+        {
+            if (listaUlaganja.Count > 0)
+            {
+                Dictionary<string, double> PR = new Dictionary<string, double>();
+                foreach (Ulaganje ulaganje in listaUlaganja)
+                {
+                    if (PR.Keys.Contains(ulaganje.SifraKriptovalute))
+                    {
+                        PR[ulaganje.SifraKriptovalute] += ulaganje.Iznos;
+                    }
+                    else
+                    {
+                        PR[ulaganje.SifraKriptovalute] = ulaganje.Iznos;
+                    }
+                }
+                double maxIznos = PR.Values.Max();
+                string valutaSaNajvecimUlogom = "";
+                foreach (string SifraKriptovalute in PR.Keys)
+                {
+                    if (PR[SifraKriptovalute] == maxIznos)
+                    {
+                        valutaSaNajvecimUlogom = SifraKriptovalute;
+                    }
+                }
+                return valutaSaNajvecimUlogom;
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+        public override string ToString()
+        {
+            return ime + " " + prezime;
+        }
+    }
+}
--- Ulaganje.cs
+++ Ulaganje.cs
@@ -6,104 +6,95 @@ using System.Threading.Tasks;
 
 namespace MalaKriptoEvidencija2022
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    namespace MalaKriptoEvidencija2022
+    class Ulaganje
     {
-        class Ulaganje
+        private int redniBroj;
+
+        private string sifraKriptovalute;
+
+        private double iznos;
+
+        private double cena;
+
+        private string mesec;
+
+        public Ulaganje(int redniBroj, string sifraKriptovalute, double iznos, double cena, string mesec)
         {
-            private int redniBroj;
+            this.RedniBroj = redniBroj;
+            this.SifraKriptovalute = sifraKriptovalute;
+            this.Iznos = iznos;
+            this.Cena = cena;
+            this.Mesec = mesec;
+        }
 
-            private string sifraKriptovalute;
-
-            private double iznos;
-
-            private double cena;
-
-            private string mesec;
-
-            public Ulaganje(int redniBroj, string sifraKriptovalute, double iznos, double cena, string mesec)
+        public int RedniBroj
+        {
+            get => redniBroj;
+            set
             {
-                this.RedniBroj = redniBroj;
-                this.SifraKriptovalute = sifraKriptovalute;
-                this.Iznos = iznos;
-                this.Cena = cena;
-                this.Mesec = mesec;
-            }
-
-            public int RedniBroj
-            {
-                get => redniBroj;
-                set
+                if (value < 1)
                 {
-                    if (value < 1)
-                    {
-                        throw new Exception("Redni broj ne može biti negativan broj niti 0.");
-                    }
-                    redniBroj = value;
+                    throw new Exception("Redni broj ne može biti negativan broj niti 0.");
                 }
+                redniBroj = value;
             }
+        }
 
-            public string SifraKriptovalute { get => sifraKriptovalute; set => sifraKriptovalute = value; }
+        public string SifraKriptovalute { get => sifraKriptovalute; set => sifraKriptovalute = value; }
 
-            public double Iznos
+        public double Iznos
+        {
+            get => iznos;
+            set
             {
-                get => iznos;
-                set
+                if (value < 0)
                 {
-                    if (value < 0)
-                    {
-                        throw new Exception("Iznos ne može biti negativan broj.");
-                    }
-                    iznos = value;
+                    throw new Exception("Iznos ne može biti negativan broj.");
                 }
+                iznos = value;
             }
+        }
 
-            public double Cena
+        public double Cena
+        {
+            get => cena;
+            set
             {
-                get => cena;
-                set
+                if (value < 0)
                 {
-                    if (value < 0)
-                    {
-                        throw new Exception("Cena ne može biti negativan broj.");
-                    }
-                    cena = value;
+                    throw new Exception("Cena ne može biti negativan broj.");
                 }
+                cena = value;
             }
+        }
 
-            public string Mesec
+        public string Mesec
+        {
+            get => mesec;
+            set
             {
-                get => mesec;
-                set
+                if (value != "januar"
+                    && value != "februar"
+                    && value != "mart"
+                    && value != "april"
+                    && value != "maj"
+                    && value != "jun"
+                    && value != "jul"
+                    && value != "avgust"
+                    && value != "septembar"
+                    && value != "oktobar"
+                    && value != "novembar"
+                    && value != "decembar")
                 {
-                    if (value != "januar"
-                        && value != "februar"
-                        && value != "mart"
-                        && value != "april"
-                        && value != "maj"
-                        && value != "jun"
-                        && value != "jul"
-                        && value != "avgust"
-                        && value != "septembar"
-                        && value != "oktobar"
-                        && value != "novembar"
-                        && value != "decembar")
-                    {
-                        throw new Exception("Mesec može biti: januar, februar, mart, april, maj, jun, jul, avgust, septembar, oktobar, novembar, decembar");
-                    }
-                    mesec = value;
+                    throw new Exception("Mesec može biti: januar, februar, mart, april, maj, jun, jul, avgust, septembar, oktobar, novembar, decembar");
                 }
+                mesec = value;
             }
+        }
 
-            public override string ToString()
-            {
-                return redniBroj + "\t" + sifraKriptovalute + "\t" + iznos + "\t" + cena + "\t" + mesec;
-            }
+        public override string ToString()
+        {
+            return redniBroj + "\t" + sifraKriptovalute + "\t" + iznos + "\t" + cena + "\t" + mesec;
         }
     }
 }
