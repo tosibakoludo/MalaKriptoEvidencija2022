84c9ffb2802fe2ffb8c8f16005c1ae34ac20f796
Tamara Brkic <brkictamara7@gmail.com>
2022-03-19 11:56:36 +0100

    Dodata apstraktna klasa Valuta.

---
+++ Valuta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MalaKriptoEvidencija2022
+{
+    abstract class Valuta
+    {
+        protected string sifra;
+
+        protected string naziv;
+
+        public string Sifra { get => sifra; set => sifra = value; }
+
+        public string Naziv { get => naziv; set => naziv = value; }
+
+        public override string ToString()
+        {
+            return naziv + " (" + sifra + ")\t=>";
+        }
+    }
+}
